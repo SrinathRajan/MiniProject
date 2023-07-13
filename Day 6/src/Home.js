e70863a7919c912a814f011c8f4ae55f6a934ca5
SrinathRajan <126069805+SrinathRajan@users.noreply.github.com>
2023-07-13 14:44:35 +0530

    Add files via upload

---
+++ Day 6/src/Home.js
@@ -0,0 +1,51 @@
+import React from "react";
+import './Pages/Home.css';
+import { Link } from "react-router-dom";
+
+
+function Home(){
+    return(
+    <div className="navbar">
+            <h2>Welcome To Logistics Management System</h2>
+            <div className="nav">
+             <Link to="/About" className="About">
+                <h2>About</h2>
+             </Link>
+             <Link to="/Contact" className="Contact">
+                <h2>Contact</h2>
+             </Link>
+             <Link to="/Product" className="Product">
+                <h2>Product Details</h2>
+             </Link>
+             <Link to="/" className="Logout">
+                <h2>Logout</h2>
+             </Link>
+
+            </div>
+        </div>
+ 
+      
+        // <nav>
+        // <ul>
+        //  <li>
+        // <a href="#"> About Us </a>
+        // </li>
+        // <li>
+        // <a href="#"> Contact Us </a>
+        // </li>
+        // <li> <a href="#"> Product Details </a>
+        // </li>
+        /* <li>
+        <a href="#"> Join Us </a>
+        </li>  */
+        // </ul>
+        // </nav> 
+        // <h1>Welcome To Home Page</h1>
+    
+    );
+
+}
+export default Home;
+
+
+    
